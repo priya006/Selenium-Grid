d2cc09edec64837487f20d65e550e6fd219f5517
pboopathi <pboopathi@ebay.com>
2020-11-11 20:16:39 -0800

    Added TestNG file to execute selenium Test in AWS using Nodes and selenium hub in containers

--- src/main/java/SeleniumTestInAWSDocker.java
+++ src/main/java/SeleniumTestInAWSDocker.java
@@ -9,8 +9,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
